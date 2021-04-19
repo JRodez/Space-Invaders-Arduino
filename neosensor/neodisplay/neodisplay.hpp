28f60bfda613d1cc7fc2f09ce49e7863eee6cf9d
JRodez <80471345+JRodez@users.noreply.github.com>
2021-04-19 23:45:39 +0200

    hello world auto detection

---
+++ neosensor/neodisplay/neodisplay.hpp
@@ -0,0 +1,30 @@
+#ifndef _NEODISPLAY_H_
+#define _NEODISPLAY_H_
+#include <U8g2lib.h>
+#include <Arduino.h>
+#include <stdint.h>
+
+#define SDA 21
+#define SCL 22
+
+// #ifdef __cplusplus
+// extern "C"
+// {
+// #endif
+
+class Neodisplay : public U8G2
+{
+public:
+	Neodisplay(const u8g2_cb_t *, uint8_t, uint8_t, uint8_t);
+	Neodisplay(void);
+
+	boolean isConnected(void);
+
+private:
+	boolean connected = false;
+};
+// #ifdef __cplusplus
+// }
+// #endif
+
+#endif
--- neosensor/neosensor.ino
+++ neosensor/neosensor.ino
@@ -2,11 +2,15 @@
 #include <U8g2lib.h>
 #include <HardwareSerial.h>
 #include "sprites/sprites.h"
-
+#include "neodisplay/neodisplay.hpp"
+#include "neocampus_i2c.h"
+#include <Wire.h>
 #define SDA 21
 #define SCL 22
+// #define U8G2_128X64_CONSTRUCTOR U8G2_SH1106_128X64_NONAME_F_HW_I2C
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C disp(U8G2_R0, U8X8_PIN_NONE, SCL, SDA); // OLEDs without Reset of the Display
+uint8_t i = 0;
 
 void setup()
 {
@@ -17,26 +21,27 @@ void setup()
 		Serial.print(".");
 		delay(1000);
 	}
-	Serial.println();
 
-	disp.begin();
-	disp.enableUTF8Print();
+	Neodisplay neodisp;
+
+	neodisp.begin();
+	neodisp.enableUTF8Print();
+
 	Serial.println("Début :");
 
-	disp.clearBuffer();				  // on efface ce qui se trouve déjà dans le buffer
-	disp.setFont(u8g2_font_7x13B_tf); // choix d'une police de caractère
-	disp.setCursor(25, 40);
-	disp.print("Hello World!");
-	disp.sendBuffer();
-	delay(5000);
+	neodisp.clearBuffer();				 // on efface ce qui se trouve déjà dans le buffer
+	neodisp.setFont(u8g2_font_7x13B_tf); // choix d'une police de caractère
+	neodisp.setCursor(25, 40);
+	neodisp.print("Hello World  !");
+	neodisp.sendBuffer();
+	// delay(5000);
 }
 
-uint8_t i = 0;
 void loop()
 {
-	disp.clearBuffer(); // on efface ce qui se trouve déjà dans le buffer
-	disp.drawXBMP(0, 0, alien_width, alien_height, alienA[i]);
-	disp.sendBuffer();
+	// disp.clearBuffer(); // on efface ce qui se trouve déjà dans le buffer
+	// disp.drawXBMP(0, 0, alien_width, alien_height, alienA[i]);
+	// disp.sendBuffer();
 	delay(500);
 	i = (i + 1) % 2;
 }
